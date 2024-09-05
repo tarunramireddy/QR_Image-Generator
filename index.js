29573ced7f9c7ec7cad5abc301a99f71682aa9e5
tarunramireddy <116076981+tarunramireddy@users.noreply.github.com>
2024-09-04 17:54:42 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+import inquirer from 'inquirer';
+import qr from 'qr-image';
+import fs from 'fs';
+import { throws } from 'assert';
+
+inquirer
+  .prompt([
+    {
+      message: "Type in your url: ",
+      name: "URL"
+    }
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+
+    fs.writeFile("url.txt",url,(err)=>{
+      if (err) throw err;
+      console.log("The file has been saved!");
+    })
+
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
